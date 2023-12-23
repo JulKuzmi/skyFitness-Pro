89531452d1eab6ccef0f69fb9ba50cb4de7d48ed
JulKuzmi <enchantress000@mail.ru>
2023-12-23 17:58:09 +0300

    setting config firebase and env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyCSXqVNlmo3xb3VGA3PcCjrhxInP0F5DiM',
-    authDomain: 'skyfitness-pro.firebaseapp.com',
-    databaseURL:
-        'https://skyfitness-pro-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'skyfitness-pro',
-    storageBucket: 'skyfitness-pro.appspot.com',
-    messagingSenderId: '884609337340',
-    appId: '1:884609337340:web:0cdfed2e01d6c3aff15479',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KYE,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_ID_API,
 }
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
