1584d37d87560d7e8a37c655230e8c0614728fc7
JulKuzmi <enchantress000@mail.ru>
2023-12-23 16:45:00 +0300

    add file for firebase and add app

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyCSXqVNlmo3xb3VGA3PcCjrhxInP0F5DiM',
+    authDomain: 'skyfitness-pro.firebaseapp.com',
+    databaseURL:
+        'https://skyfitness-pro-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'skyfitness-pro',
+    storageBucket: 'skyfitness-pro.appspot.com',
+    messagingSenderId: '884609337340',
+    appId: '1:884609337340:web:0cdfed2e01d6c3aff15479',
+}
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
