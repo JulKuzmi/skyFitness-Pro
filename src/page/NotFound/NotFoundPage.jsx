26fd2b27670fae6c72ba77f5f0f6ccc690e5f8c1
JulKuzmi <enchantress000@mail.ru>
2024-01-09 16:21:44 +0300

    not found and style

--- src/page/NotFound/NotFoundPage.jsx
+++ src/page/NotFound/NotFoundPage.jsx
@@ -1,3 +1,19 @@
+import * as S from './NotFoundPage.style'
+import { Link } from 'react-router-dom'
+
 export const NotFoundPage = () => {
-    return <div>Страница не найдена, оошибка 404</div>
+    return (
+        <S.notFound>
+            <S.notFoundCode>404</S.notFoundCode>
+            <S.notFoundTextWrap>
+                <S.notFoundText>Страница не найдена</S.notFoundText>
+            </S.notFoundTextWrap>
+            <S.notFoundDescription>
+                Возможно, она была удалена или перенесена на другой адрес
+            </S.notFoundDescription>
+            <Link to="/">
+                <S.notFoundButton>Вернуться на главную</S.notFoundButton>
+            </Link>
+        </S.notFound>
+    )
 }
